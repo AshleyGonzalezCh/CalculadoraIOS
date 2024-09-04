3c7d715be0a2158a2fb7be247da66086dfade715
alumno <alumno@iMac-V108-12.local>
2024-09-04 10:50:04 -0600

    [ACTUALIZACIÓN] Se agregó código

--- P2. Enlaces/ViewController.swift
+++ P2. Enlaces/ViewController.swift
@@ -8,17 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var texto: UILabel!
-    @IBOutlet weak var botob_interactuar_blanco: UIButton!
-    @IBOutlet weak var boton_interactuar: UIButton!
+    //ESPACIO PARA DECLARAR BOTONES O ELEMENTOS VISUALES
+    @IBOutlet weak var Etiqueta: UILabel!
+    //ESPACIO PARA DECLARAR VARIABLES
+    var InputActual: Double = 0
+    var InputAnterior: Double = 0
+    var Operador: String = ""
+    var OpSeleccionada : Bool = false
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func Accion_boton(_ sender: Any) {
-        texto.text = "Hola"
+    @IBAction func Boton_Presionado(_ sender: UIButton) {
+        let numero = sender.currentTitle!
+        if OpSeleccionada{
+            Etiqueta.text = numero
+            OpSeleccionada = false
+        }
+        else {
+            Etiqueta.text = Etiqueta.text! + numero
+        }
+        InputActual = Double(Etiqueta.text!)!
+    }
+    
+    @IBAction func OpSel (_ sender: UIButton){
+        Operador = sender.currentTitle!
+        InputAnterior = InputActual
+        OpSeleccionada = true
+    }
+    
+    
+    @IBAction func Limpiar(_ sender: Any) {
+        InputActual = 0;
+        InputAnterior = 0;
+        Operador = "";
+        
+    }
+    
+    @IBAction func Resultado(_ sender: UIButton) {
+        if Operador == "+"{
+            Etiqueta.text = String(InputAnterior+InputActual)
+        }
+        else if Operador == "-"{
+                Etiqueta.text = String(InputAnterior-InputActual)
+        }
+        
+        else if Operador == "*"{
+                Etiqueta.text = String(InputAnterior*InputActual)
+        }
+        else if Operador == "/"{
+                Etiqueta.text = String(InputAnterior/InputActual)
+        }
     }
     
 }
